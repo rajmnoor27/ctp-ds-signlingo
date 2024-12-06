5e6ea2525c714ecf5f62ec6e428b2da9425db1c4
Daniel Chen <113078548+dchen024@users.noreply.github.com>
2024-12-06 04:22:47 -0500

    fix: styling

--- frontend/components/ui/quizCard.tsx
+++ frontend/components/ui/quizCard.tsx
@@ -14,28 +14,30 @@ export function QuizCard({ id, title }: QuizCardProps) {
   return (
     <div className='w-full h-full'>
       <div className='flex items-center justify-between bg-[#6BA6FF] p-4 rounded-lg min-w-[700px] max-w-[800px] min-h-[200px] px-24'>
-        <div className='flex flex-col text-white gap-4'>
+        <div className='flex flex-col gap-4 text-white'>
           <h2 className='text-4xl font-bold'>{title}</h2>
 
+          <div className='flex gap-2'>
             {quizData[id - 1].letters.map((letter) => (
               <p key={letter} className='text-lg font-bold'>
                 {letter}
               </p>
-          ))}
-        </div>
-        <Link href={`/quiz/${id}`}>
-          <Button className='bg-feather-green drop-shadow-2xl hover:bg-feather-green/80'>
+            ))}
+          </div>
+          <Link href={`/quiz/${id}`}>
+            <Button className='bg-feather-green drop-shadow-2xl hover:bg-feather-green/80'>
               Start
             </Button>
           </Link>
         </div>
         <Image
-          className='rounded-lg'
           src='/chicken1.png'
           width={144}
           height={144}
           alt='Picture of chicken with thumbs up'
-      />
+          className='rounded-lg'
+        />
+      </div>
     </div>
   );
 }
